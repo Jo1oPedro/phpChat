2d4abb27f67903a73b8165cb335702616319d2a1
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-06-18 22:41:34 -0300

    Adiciona comunicação com banco de dados e comunicação com usuarios especificos

--- src/UserCounter.php
+++ src/UserCounter.php
@@ -4,19 +4,24 @@ namespace Websocket\App;
 
 use Ratchet\ConnectionInterface;
 use Ratchet\MessageComponentInterface;
+use Websocket\App\database\Connection;
 
 class UserCounter implements MessageComponentInterface
 {
     protected $client;
+    private \PDO $connection;
 
     public function __construct()
     {
         $this->client = new \SplObjectStorage();
+        $this->connection = Connection::getInstance();
     }
 
     public function onOpen(ConnectionInterface $conn)
     {
-        $this->client->attach($conn);
+        $queryString = $conn->httpRequest->getUri()->getQuery();
+        parse_str($queryString, $queryParams);
+        $this->client->attach($conn, (int) $queryParams["userId"]);
     }
 
     public function onClose(ConnectionInterface $conn)
@@ -31,8 +36,23 @@ class UserCounter implements MessageComponentInterface
 
     public function onMessage(ConnectionInterface $from, $msg)
     {
+        $decodedMessage = json_decode($msg);
+        $decodedMessage->user_id = $this->client[$from];
+        $to = $decodedMessage->to;
+
+        $statement = $this->connection->prepare("INSERT INTO user_messages (from_user_id, to_user_id, message_content) VALUES (:from_id, :to_id, :message_content)");
+        $statement->execute([
+            "from_id" => $this->client[$from],
+            "to_id" => $to,
+            "message_content" => $decodedMessage->message
+        ]);
+
         foreach($this->client as $client) {
-            $client->send($msg);
+            var_dump(json_encode($decodedMessage));
+            if($this->client[$client] === $to || $this->client[$client] === $decodedMessage->user_id) {
+                var_dump($this->client[$client]);
+                $client->send(json_encode($decodedMessage));
+            }
         }
     }
 
---
+++ src/database/Connection.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Websocket\App\database;
+
+use PDO;
+
+class Connection
+{
+    private const OPTIONS = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ];
+
+    private static ?PDO $pdo = null;
+    private function __construct()
+    {
+    }
+
+    public static function getInstance(): PDO
+    {
+        if(is_null(self::$pdo)) {
+            try {
+                self::$pdo = new \PDO(
+                    "mysql:host=127.0.0.1:5306;dbname=laravel",
+                    "user",
+                    "secret",
+                    self::OPTIONS
+                );
+            } catch (\PDOException $exception) {
+                die(var_dump($exception));
+            }
+        }
+        return self::$pdo;
+    }
+}
